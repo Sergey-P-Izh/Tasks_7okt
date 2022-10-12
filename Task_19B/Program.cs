fd42265b081bf94d9dfd0f26ed85225d3dfeab74
Sergey <u3jiom@bk.ru>
2022-10-12 21:33:43 +0400

    Добавлена задача 19B

---
+++ Task_19B/Program.cs
@@ -0,0 +1,32 @@
+﻿static void Main(string[] args)
+{
+    string s = "Ароза упала на лапу Азора";
+    string ss = "Нажал кабан на баклажан";
+    string sss = string.Empty;
+    string ssss = string.Empty;
+
+    for (int x = MyLength(s) - 1 ; x >= 0; x--)
+    {
+        if (s[x].ToString() != " ")
+        {
+            sss += s[x].ToString().ToLower();
+        }
+    }
+
+    for (int a = 0; a < MyLength(s); a++)
+    {
+        if (s[a].ToString() != " ")
+        {
+             ssss += s[a].ToString().ToLower();
+        }
+    }
+    if (sss = ssss)
+    {
+        Console.WriteLine("TRUE");
+    }
+    else 
+    {
+       Console.WriteLine("FALSE"); 
+    }
+}
+ Console.WriteLine(sss); 
